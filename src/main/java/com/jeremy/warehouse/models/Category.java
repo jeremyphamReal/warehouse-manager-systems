bfa42a8374c33ecb9046b85fb6757ba37d5b8549
JeremyPhambinh <phambinu223@gmail.com>
2026-04-25 17:35:52 +0100

    tao class category

---
+++ src/main/java/com/jeremy/warehouse/models/Category.java
@@ -0,0 +1,43 @@
+package com.jeremy.warehouse.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Category {
+    private Long id;
+    private String name;
+    private String description;
+    private Product product;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
