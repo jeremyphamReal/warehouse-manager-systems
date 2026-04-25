d791fae43ea7e4ab20979e0d31d2c1b8d3935c09
JeremyPhambinh <phambinu223@gmail.com>
2026-04-25 15:44:28 +0100

    smart-inventory-systems

---
+++ src/test/java/com/jeremy/warehouse/WarehouseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeremy.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
