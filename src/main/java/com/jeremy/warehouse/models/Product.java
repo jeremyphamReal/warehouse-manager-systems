f03aee223d69b508f0903fcbf9e8783622b9ef1f
JeremyPhambinh <phambinu223@gmail.com>
2026-04-25 17:38:39 +0100

    tao class product

---
+++ src/main/java/com/jeremy/warehouse/models/Product.java
@@ -0,0 +1,90 @@
+package com.jeremy.warehouse.models;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class Product {
+    private Long id;
+    private String name;
+    private String SKU;
+    private Double price;
+    private int Quantity;
+    private String description;
+    private Date createAt;
+    private Date updateAt;
+    private Category category;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSKU() {
+        return SKU;
+    }
+
+    public void setSKU(String SKU) {
+        this.SKU = SKU;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        Quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
