7cef812e8670fb9d8dc8fe53b523796a03d94f4c
JeremyPhambinh <phambinu223@gmail.com>
2026-06-06 05:14:23 +0100

    sua lai doi tu class sang interface voi <jparepo, long>

--- src/main/java/com/jeremy/warehouse/repository/CategoryRepo.java
+++ src/main/java/com/jeremy/warehouse/repository/CategoryRepo.java
@@ -2,41 +2,53 @@ package com.jeremy.warehouse.repository;
 
 import com.jeremy.warehouse.models.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public class CategoryRepo {
-    private JdbcTemplate jdbc;
+public interface CategoryRepo extends JpaRepository<Category,Long> {
+//    private JdbcTemplate jdbc;
+//
+//    public JdbcTemplate getJdbc() {
+//        return jdbc;
+//    }
+//
+//    @Autowired
+//    public void setJdbc(JdbcTemplate jdbc) {
+//        this.jdbc = jdbc;
+//    }
+//
+//    //Read
+//    public List<Category> findAll() {
+//        String query =  "select * from category";
+//
+//        return jdbc.query(query, (rs, rowNum) ->{
+//            Category category = new Category();
+//
+//            category.setId(rs.getLong("Id"));
+//            category.setName(rs.getString("Name"));
+//            category.setDescription(rs.getString("Description"));
+//
+//            return category;
+//        });
+//    }
+//
+//    //Created
+//    public void save(Category category) {
+//        String query = "insert into category (id, name, description) values (?,?,?)";
+//        int rows = jdbc.update(query, category.getId(), category.getName(), category.getDescription());
+//        System.out.println(rows + " effected");
+//    }
+//
+//    //Deleted
+//    public int deleteById(String id){
+//        String query  = "DELETE FROM category WHERE id =?";
+//        int rows = jdbc.update(query, id);
+//        System.out.println(rows);
+//        return rows;
+//    }
 
-    public JdbcTemplate getJdbc() {
-        return jdbc;
-    }
-
-    @Autowired
-    public void setJdbc(JdbcTemplate jdbc) {
-        this.jdbc = jdbc;
-    }
-
-    public List<Category> findAll() {
-        String query =  "select * from category";
-
-        return jdbc.query(query, (rs, rowNum) ->{
-            Category category = new Category();
-
-            category.setId(rs.getLong("Id"));
-            category.setName(rs.getString("Name"));
-            category.setDescription(rs.getString("Description"));
-
-            return category;
-        });
-    }
-
-    public void save(Category category) {
-        String query = "insert into category (id, name, description) values (?,?,?)";
-        int rows = jdbc.update(query, category.getId(), category.getName(), category.getDescription());
-        System.out.println(rows + " effected");
-    }
 }
