089cc93e69748749e70948b6b3405c83ef0a3f86
jeremypham <phambinu223@gmail.com>
2026-08-19 18:44:19 +0700

    cap nhat lai user

--- src/main/java/com/jeremy/warehouse/repository/UserRepo.java
+++ src/main/java/com/jeremy/warehouse/repository/UserRepo.java
@@ -4,7 +4,11 @@ import com.jeremy.warehouse.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
     User findByUsername(String username);
+
+    List<User> findByRole(String role);
 }
--- src/main/java/com/jeremy/warehouse/service/UserService.java
+++ src/main/java/com/jeremy/warehouse/service/UserService.java
@@ -5,6 +5,9 @@ import com.jeremy.warehouse.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -12,8 +15,29 @@ public class UserService {
     private UserRepo repo;
     private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(10);
 
-    public User save(User user) {
+    public User save(@RequestBody User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         return repo.save(user);
+    }
+
+    public List<User> findAllStaff() {
+        List<User> staff= repo.findByRole("STAFF");
+        if(staff.isEmpty()){
+            throw new RuntimeException("Không tìm thấy danh sách staff trong hệ thống");
+        }
+        return staff;
+    }
+
+    public void deleteById(Long id) {
+        repo.deleteById(id);
+    }
+
+    public User update(Long id, User user) {
+        User existingUser = repo.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        if(existingUser.getPassword()!=null && existingUser.getPassword().isEmpty()){
+            existingUser.setPassword(user.getPassword());
+        }
+        existingUser.setRole(user.getRole());
+        return repo.save(existingUser);
     }
 }
